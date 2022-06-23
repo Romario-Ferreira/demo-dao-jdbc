d679386ff2d1346f2c9c18431f1724bcb03524c6
Romário Ferreira <romarioffranca@gmail.com>
2022-06-23 11:09:02 -0300

    Seller/Department classes created

--- src/db/DB.java
+++ src/db/DB.java
@@ -14,23 +14,22 @@ public class DB {
 	private static Connection conn = null;
 	
 	
-	public static Connection getConnection() {
+	public static Connection getConnection() {	
 		if(conn==null) {
-		try {
-			Properties prop = loadProperties();
-			String url = prop.getProperty("dburl");
-			conn = DriverManager.getConnection(url, prop);			
-		}
-		catch(SQLException e) {
-			throw new DbException(e.getMessage());
-		}
-		
+			try {
+				Properties prop = loadProperties();
+				String url = prop.getProperty("dburl");
+				conn = DriverManager.getConnection(url,prop);
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
 		}
 		return conn;
 	}
-	
+
 	public static void closeConnection() {
-		if(conn!=null) {
+		if(conn != null) {
 			try {
 				conn.close();
 			} catch (SQLException e) {
@@ -38,38 +37,36 @@ public class DB {
 			}
 		}
 	}
-	
-	
+
 	private static Properties loadProperties() {
-		try(FileInputStream fs = new FileInputStream("db.properties")){
+		try (FileInputStream fs = new FileInputStream("db.properties")){
 			Properties prop = new Properties();
 			prop.load(fs);
 			return prop;
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
-	
+
 	public static void closeStatement(Statement st) {
-		if (st!=null) {
+		if (st != null) {
 			try {
 				st.close();
-			} catch (SQLException e) {
+			}
+			catch(SQLException e) {
 				throw new DbException(e.getMessage());
 			}
-		}
+		}	
 	}
-	
 	
 	public static void closeResultSet(ResultSet rs) {
-		if (rs!=null) {
+		if(rs!=null) {
 			try {
 				rs.close();
-			} catch (SQLException e) {
+			} 
+			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
-	
 }

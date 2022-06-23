50974e805ecdb46e4482852215264db151363ddd
Romário Ferreira <romarioffranca@gmail.com>
2022-06-23 15:29:21 -0300

    findByDepartment implementation on SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.sql.Connection;
+import java.util.List;
 
 import db.DB;
 import model.dao.DaoFactory;
@@ -21,12 +22,19 @@ public class Program {
 
 		System.out.println("Banco de dados conectado");
 		
+		System.out.println();
 		System.out.println("===== TESTE 1 FindById =====");
 		seller = sellerDao.findById(3);
 		dep = depDao.findById(3);
 		System.out.println(seller);
 		System.out.println(dep);
 		
+		System.out.println();
+		System.out.println("===== TESTE 2 FindByDepartment =====");
+		List<Seller> sellerList = sellerDao.findByDepartment(new Department(4, null));
+		for (Seller s : sellerList) {
+			System.out.println(s);
+		}
 		
 	}
 }
